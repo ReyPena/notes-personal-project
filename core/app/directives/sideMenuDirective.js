80463be9e95e95c00a50094b14fac364a3df3528
ReyPena <zonickblack@gmail.com>
2016-06-15 19:05:15 -0600

    Create directive for the side menu.

---
+++ core/app/directives/sideMenuDirective.js
@@ -0,0 +1,6 @@
+angular.module("notes").directive("sideMenu", function () {
+  return {
+    restrict: "E",
+    templateUrl:"../views/directivesViews/sideMenu.html"
+  };
+});
--- public/scripts/mainScripts.js
+++ public/scripts/mainScripts.js
@@ -28431,91 +28431,6 @@ angular.module("notes").controller("registerCtrl", ["$scope", "registerService",
   };
 }]);
 
-angular.module("notes").config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state("home", {
-      url: "/home",
-      templateUrl: "../views/routeViews/home/home.html"
-    })
-    .state("login", {
-      url: "/login",
-      templateUrl: "../views/routeViews/auth/login.html"
-      // controller: "loginCtrl"
-    })
-    .state("register", {
-      url: "/register",
-      templateUrl: "../views/routeViews/auth/register.html"
-      // controller: "registerCtrl"
-    })
-    .state("profile", {
-      url: "/profile",
-      templateUrl: "../views/routeViews/profile/profile.html"
-      // resolve: {
-      //   token: function(tokenFactory, $state) {
-      //     var token = tokenFactory.getToken();
-      //     if (!token) {
-      //       $state.go("/#/home");
-      //     }
-      //   }
-      // }
-    })
-    .state("books", {
-      url: "/books",
-      templateUrl: "../views/routeViews/books/books.html"
-      // controller: "booksCtrl",
-      // resolve: {
-      //   token: function(tokenFactory, $state) {
-      //     var token = tokenFactory.getToken();
-      //     if (!token) {
-      //       $state.go("/#/home");
-      //     }
-      //   }
-      // }
-    })
-    .state("book", {
-      url: "/book/:id",
-      templateUrl: "../views/routeViews/book/book.html"
-      // controller: "bookCtrl",
-      // resolve: {
-      //   token: function(tokenFactory, $state) {
-      //     var token = tokenFactory.getToken();
-      //     if (!token) {
-      //       $state.go("/#/home");
-      //     }
-      //   }
-      // }
-    })
-    .state("note", {
-      url: "/note/:id",
-      templateUrl: "../views/routeViews/note/note.html"
-      // controller: "noteCtrl",
-      // resolve: {
-      //   token: function(tokenFactory, $state) {
-      //     var token = tokenFactory.getToken();
-      //     if (!token) {
-      //       $state.go("/#/home");
-      //     }
-      //   }
-      // }
-    })
-    .state("editor", {
-      url: "/editor",
-      templateUrl: "../views/routeViews/editor/editor.html"
-      // controller: "editorCtrl",
-      // resolve: {
-      //   token: function(tokenFactory, $state) {
-      //     var token = tokenFactory.getToken();
-      //     if (!token) {
-      //       $state.go("/#/home");
-      //     }
-      //   }
-      // }
-    });
-
-  $urlRouterProvider
-    .otherwise("/home");
-}]);
-
 angular.module("notes").directive("codeEditDirective", function() {
   return {
     restrict: "AE",
@@ -28552,6 +28467,13 @@ angular.module("notes").directive("menuDirective", function () {
     restrict: "E",
     templateUrl:"../views/directivesViews/menu.html",
     controller: "menuCtrl"
+  };
+});
+
+angular.module("notes").directive("sideMenu", function () {
+  return {
+    restrict: "E",
+    templateUrl:"../views/directivesViews/sideMenu.html"
   };
 });
 
@@ -28760,4 +28682,89 @@ angular.module("notes").factory("tokenInterceptor", ["tokenFactory", function (t
     }
     return config;
   }
+}]);
+
+angular.module("notes").config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state("home", {
+      url: "/home",
+      templateUrl: "../views/routeViews/home/home.html"
+    })
+    .state("login", {
+      url: "/login",
+      templateUrl: "../views/routeViews/auth/login.html"
+      // controller: "loginCtrl"
+    })
+    .state("register", {
+      url: "/register",
+      templateUrl: "../views/routeViews/auth/register.html"
+      // controller: "registerCtrl"
+    })
+    .state("profile", {
+      url: "/profile",
+      templateUrl: "../views/routeViews/profile/profile.html"
+      // resolve: {
+      //   token: function(tokenFactory, $state) {
+      //     var token = tokenFactory.getToken();
+      //     if (!token) {
+      //       $state.go("/#/home");
+      //     }
+      //   }
+      // }
+    })
+    .state("books", {
+      url: "/books",
+      templateUrl: "../views/routeViews/books/books.html"
+      // controller: "booksCtrl",
+      // resolve: {
+      //   token: function(tokenFactory, $state) {
+      //     var token = tokenFactory.getToken();
+      //     if (!token) {
+      //       $state.go("/#/home");
+      //     }
+      //   }
+      // }
+    })
+    .state("book", {
+      url: "/book/:id",
+      templateUrl: "../views/routeViews/book/book.html"
+      // controller: "bookCtrl",
+      // resolve: {
+      //   token: function(tokenFactory, $state) {
+      //     var token = tokenFactory.getToken();
+      //     if (!token) {
+      //       $state.go("/#/home");
+      //     }
+      //   }
+      // }
+    })
+    .state("note", {
+      url: "/note/:id",
+      templateUrl: "../views/routeViews/note/note.html"
+      // controller: "noteCtrl",
+      // resolve: {
+      //   token: function(tokenFactory, $state) {
+      //     var token = tokenFactory.getToken();
+      //     if (!token) {
+      //       $state.go("/#/home");
+      //     }
+      //   }
+      // }
+    })
+    .state("editor", {
+      url: "/editor",
+      templateUrl: "../views/routeViews/editor/editor.html"
+      // controller: "editorCtrl",
+      // resolve: {
+      //   token: function(tokenFactory, $state) {
+      //     var token = tokenFactory.getToken();
+      //     if (!token) {
+      //       $state.go("/#/home");
+      //     }
+      //   }
+      // }
+    });
+
+  $urlRouterProvider
+    .otherwise("/home");
 }]);
